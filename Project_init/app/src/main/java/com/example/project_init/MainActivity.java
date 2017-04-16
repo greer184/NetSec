4de4002d4a548ae265ae82d15962a882e2f25eec
William Greer <william3-3052@sbcglobal.net>
2017-04-16 16:12:52 -0400

    Bluetooth pairs with other devices

--- Project_init/app/src/main/java/com/example/project_init/MainActivity.java
+++ Project_init/app/src/main/java/com/example/project_init/MainActivity.java
@@ -2,44 +2,131 @@ package com.example.project_init;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
+import java.util.Set;
 
 public class MainActivity extends Activity {
     Button b1;
     private BluetoothAdapter blueAdapt;
+    public static String EXTRA_DEVICE_ADDRESS = "device_address";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Set button to begin connection
-        b1 = (Button) findViewById(R.id.button);
+        if(blueAdapt == null){
+            Toast.makeText(getApplicationContext(), "Unable to use Bluetooth", Toast.LENGTH_LONG).show();
+        } else {
 
+            // Turn on Bluetooth if off
+            if (!blueAdapt.isEnabled()) {
+                Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(turnOn, 0);
+                Toast.makeText(getApplicationContext(),
+                        "Bluetooth enabled", Toast.LENGTH_LONG).show();
+            }
+
+        }
+
+        // Set button to allow Bluetooth connection
+        b1 = (Button) findViewById(R.id.buttonD);
     }
 
-    // Verify whether or not bluetooth is enabled
-    public void on(View v){
+    // Bluetooth code for discovering and pairing Bluetooth devices
+    public void discovery(View v){
 
         // Setup adapter
         blueAdapt = BluetoothAdapter.getDefaultAdapter();
 
-        // Initial Bluetooth check
-        if(blueAdapt == null){
-            Toast.makeText(getApplicationContext(),"Unable to use Bluetooth", Toast.LENGTH_LONG).show();
-        } else {
-            if (!blueAdapt.isEnabled()) {
-                Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                startActivityForResult(turnOn, 0);
-                Toast.makeText(getApplicationContext(), "Bluetooth enabled" ,Toast.LENGTH_LONG).show();
-            } else {
-                Toast.makeText(getApplicationContext(), "Bluetooth already on" , Toast.LENGTH_LONG).show();
+        // Stop any attempts at discovery
+        if (blueAdapt.isDiscovering()){
+            blueAdapt.cancelDiscovery();
+        }
+
+        // If there are already paired devices, get information from here
+        Set<BluetoothDevice> paired = blueAdapt.getBondedDevices();
+
+        if (paired.size() > 0) {
+            for (BluetoothDevice device : paired) {
+                String stuff = device.getName() + "\n" + device.getAddress();
+                Toast.makeText(getApplicationContext(), stuff, Toast.LENGTH_LONG).show();
             }
+        } else {
+
+            // Start fresh discovery
+            blueAdapt.startDiscovery();
+
+            // Register for broadcasts when a device is discovered.
+            IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+            registerReceiver(mReceiver, filter);
+
         }
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // End discovery process
+        if (blueAdapt != null) {
+            blueAdapt.cancelDiscovery();
+        }
+
+        // Clear listeners
+        this.unregisterReceiver(mReceiver);
+    }
+
+    private AdapterView.OnItemClickListener deviceListener
+            = new AdapterView.OnItemClickListener() {
+        public void onItemClick(AdapterView<?> av, View v, int arg2, long arg3) {
+
+            // Cancel discovery because we are connecting
+            blueAdapt.cancelDiscovery();
+
+            // Get the device MAC address from the view
+            String info = ((TextView) v).getText().toString();
+            String address = info.substring(info.length() - 17);
+
+            // Create the result Intent and include the MAC address
+            Intent intent = new Intent();
+            intent.putExtra(EXTRA_DEVICE_ADDRESS, address);
+
+            // Set result and finish this Activity
+            setResult(Activity.RESULT_OK, intent);
+            finish();
+        }
+    };
+
+    // The BroadcastReceiver that listens for discovered devices
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            // When discovery finds a device
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+
+                // Get the BluetoothDevice object from the Intent
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+                // If it's already paired, skip it, because it's been listed already
+                if (device.getBondState() != BluetoothDevice.BOND_BONDED) {
+                    String stuff = device.getName() + "\n" + device.getAddress();
+                    Toast.makeText(getApplicationContext(), stuff, Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    };
 }
